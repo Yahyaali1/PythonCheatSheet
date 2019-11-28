a6b312dcdedb846bf448a5dbc97fde111db9a350
Yahya.Ali <yahya.ali@arbisoft.com>
2019-11-28 19:25:42 +0500

    Removed Property getters. Added context manager for filing. Removed extra spacing from end of file

--- quizgenerator/questions.py
+++ quizgenerator/questions.py
@@ -6,15 +6,9 @@ To hold information for the Question
 class Question:
 
     def __init__(self, question_text="", answer_text=""):
-        self.__question = question_text
-        self.__answer_text = answer_text
-
-    def fetch_question(self):
-        return self.__question
-
-    def fetch_answer(self):
-        return self.__answer_text
+        self.question = question_text
+        self.answer_text = answer_text
 
     def validate_answer(self, answer_to_check=""):
-        return self.__answer_text == answer_to_check
+        return self.answer_text == answer_to_check
 
--- quizgenerator/quizer.py
+++ quizgenerator/quizer.py
@@ -9,27 +9,18 @@ import random
 
 class Quizzer:
 
-    def __init__(self, ques_file_name="", ans_file_name=""):
-
-        question_file = open(ques_file_name)
-        ans_file = open(ans_file_name)
-
-        questions = question_file.readlines()
-        answers = ans_file.readlines()
-
-        self.__questions = [Question(self.__remove_key_text(questions[i]), self.__remove_key_text(answers[i]))
-                            for i in range(len(questions))]
-
-        question_file.close()
-        ans_file.close()
+    def __init__(self, ques_file_name, ans_file_name):
+        with open(ques_file_name) as question_file, open(ans_file_name) as ans_file:
+            questions = question_file.readlines()
+            answers = ans_file.readlines()
+            self.__questions = [Question(self.__remove_key_text(questions[i]), self.__remove_key_text(answers[i]))
+                                for i in range(len(questions))]
 
     def __remove_key_text(self, item=""):
-        item = item.split(".", 1)[-1]
-        return item.strip()
+        return item.split(".", 1)[-1].strip()
 
     def __randomize_questions(self):
         random.shuffle(self.__questions)
-        pass
 
     def start_quiz(self):
         user_results = []
@@ -37,7 +28,7 @@ class Quizzer:
         print("Here is the list of Questions")
 
         for i in range(len(self.__questions)):
-            print(f'Q{i+1} - {self.__questions[i].fetch_question()} \n')
+            print(f'Q{i+1} - {self.__questions[i].question} \n')
 
         print("Now Give Answers")
         for i in range(len(self.__questions)):
@@ -47,8 +38,4 @@ class Quizzer:
         print("Now Your Results")
         for i in range(len(user_results)):
             print(f'Your ans was correct for question{i+1}' if user_results[i]
-                  else f'Wrong answer, correct ans is {self.__questions[i].fetch_answer()}')
-
-
-
-
+                  else f'Wrong answer, correct ans is {self.__questions[i].answer_text}')
