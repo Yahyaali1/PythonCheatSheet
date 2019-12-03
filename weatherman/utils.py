9d6e330a488ce5d018bb42442c9e85f8a15c1d86
Yahya Ali <l144189@lhr.nu.edu.pk>
2019-12-03 13:43:13 +0500

    Removed unsed functions

--- weatherman/utils.py
+++ weatherman/utils.py
@@ -2,11 +2,6 @@
 Helper Functions for weatherman task
 """
 
-
-def is_hotter_day(old_min_temp, old_max_temp, new_min_temp, new_max_temp):
-    return new_min_temp > old_min_temp and new_max_temp > old_max_temp
-
-
 def replace_string_in_list(list_data, old_item, new_item):
     if old_item in list_data:
         list_data[list_data.index(old_item)] = new_item
