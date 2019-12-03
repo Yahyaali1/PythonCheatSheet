91b7b5875f5992606ea9675b1bcd980309d89d28
Yahya Ali <l144189@lhr.nu.edu.pk>
2019-12-03 13:43:57 +0500

    Added exception handling for invalid dir and report type

--- weatherman/weatherman.py
+++ weatherman/weatherman.py
@@ -1,30 +1,22 @@
 import argparse
 
+from argsvalidator import DirValidator, ReportNumberValidator
 from weatherreport import WeatherReport
 
-PROGRAM_INSTRUCTION = """
-    Usage: weatherman [report_type] [data_dir]
-    [Report #]
-    1 for Annual Max/Min Temperature
-    2 for Hottest day of each year
-    
-    [data_dir]
-    Directory containing weather data files
-    """
-
-parser = argparse.ArgumentParser(description=PROGRAM_INSTRUCTION)
-parser.add_argument("report_type", type=int)
-parser.add_argument("data_dir", type=str)
+parser = argparse.ArgumentParser(epilog="For forming report on the data provided in data_dir")
+parser.add_argument("report_type", type=int, action=ReportNumberValidator, help="""1 for Annual Max/Min Temperature
+    2 for Hottest day of each year""")
+parser.add_argument("data_dir", type=str, action=DirValidator, help="Directory containing weather data files")
 
 
 def main():
-    args = parser.parse_args()
-    weather_report = WeatherReport(args.data_dir)
     try:
-        weather_report.read_data()
-    except FileNotFoundError:
-        print("invalid dir")
+        args = parser.parse_args()
+    except argparse.ArgumentTypeError as exception:
+        print(str(exception))
     else:
+        weather_report = WeatherReport(args.data_dir)
+        weather_report.read_data()
         if args.report_type == 1:
             weather_report.generate_summary_report()
         else:
