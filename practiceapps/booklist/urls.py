f2d4aec9fcd24024bf574b03f76ff911d3ba225c
Yahya.Ali <yahya.ali@arbisoft.com>
2019-12-09 14:05:20 +0500

    Add form for login and adding book.

--- practiceapps/booklist/urls.py
+++ practiceapps/booklist/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from booklist.views import HomeView
+from booklist.views import HomeView, BookView
 from django.contrib.auth.views import LoginView
 
 app_name = "booklist"
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
-    path("login", LoginView.as_view(redirect_field_name='home'), name='login'),
+    path("login", LoginView.as_view(template_name="booklist/login.html"), name='login'),
+    path("add_book", BookView.as_view(), name='add_book'),
 ]
--- practiceapps/booklist/views.py
+++ practiceapps/booklist/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from booklist.forms import BookForm
 
 
-class HomeView(View):
+class HomeView(LoginRequiredMixin, View):
+    login_url = reverse_lazy("booklist:login")
+
     def get(self, request):
+        add_book_form = BookForm(initial={'owner': request.user.id+1})
+        return render(request, template_name="booklist/booklisting.html", context={'form': add_book_form})
+
+
+class BookView(LoginRequiredMixin, View):
+    login_url = reverse_lazy("booklist:login")
+
+    def post(self, request):
+        print(request.POST)
         return HttpResponse('Welcome')
