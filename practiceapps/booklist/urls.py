1980768b5608143f1eaea8f90ea77651677bc5a5
Yahya Ali <yahya.ali@outlook.com>
2019-12-10 08:32:21 +0500

    Update booklist render to list the books.

--- practiceapps/booklist/urls.py
+++ practiceapps/booklist/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from booklist.views import HomeView, BookView
+from booklist.views import BookView
 from django.contrib.auth.views import LoginView
 
 app_name = "booklist"
 urlpatterns = [
-    path("", HomeView.as_view(), name='home'),
+    path("", BookView.as_view(), name='home'),
     path("login", LoginView.as_view(template_name="booklist/login.html"), name='login'),
-    path("add_book", BookView.as_view(), name='add_book'),
+    path("addbook", BookView.as_view(), name='add_book'),
 ]
--- practiceapps/booklist/views.py
+++ practiceapps/booklist/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from booklist.forms import BookForm
-
-
-class HomeView(LoginRequiredMixin, View):
-    login_url = reverse_lazy("booklist:login")
-
-    def get(self, request):
-        add_book_form = BookForm(initial={'owner': request.user.id+1})
-        return render(request, template_name="booklist/booklisting.html", context={'form': add_book_form})
+from booklist.models import Book
 
 
 class BookView(LoginRequiredMixin, View):
     login_url = reverse_lazy("booklist:login")
 
+    def get(self, request):
+        add_book_form = None if request.user.is_staff else BookForm(initial={'owner': request.user.id})
+        user_book_list = Book.objects.all() if request.user.is_staff else Book.objects.filter(owner__id=request.user.id)
+
+        return render(request, template_name="booklist/booklisting.html", context={'form': add_book_form,
+                                                                                   'books': user_book_list})
+
     def post(self, request):
+        book_form = BookForm(request.POST)
         print(request.POST)
-        return HttpResponse('Welcome')
+        if book_form.is_valid():
+            book_form.save()
+            return HttpResponseRedirect(reverse_lazy("booklist:home"))
+        return HttpResponse('Error')
