7421056e64df49ed62015d5a78df62b651e6145f
Yahya Ali <yahya.ali@outlook.com>
2019-12-16 08:27:55 +0500

    Add logout view

--- practiceapps/booklist/urls.py
+++ practiceapps/booklist/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from booklist.views import BookView
+from booklist.views import BookView, logout_view, delete_book_view
 from django.contrib.auth.views import LoginView
 
 app_name = "booklist"
 urlpatterns = [
-    path("", BookView.as_view(), name='home'),
+    path("home", BookView.as_view(), name='home'),
     path("login", LoginView.as_view(template_name="booklist/login.html"), name='login'),
+    path("logout", logout_view, name='logout'),
     path("addbook", BookView.as_view(), name='add_book'),
+    path("delbook", delete_book_view, name='del_book')
 ]
